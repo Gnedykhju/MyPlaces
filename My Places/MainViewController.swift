c72d82dfde65c1f71009817a81b3c7775f1bcdca
Yulia <Gnedykhju@mail.ru>
2019-08-09 20:58:50 +0300

    Initial

---
+++ My Places/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by User on 09/08/2019.
+//  Copyright © 2019 Yulia Gnedykh. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+     let restaurantNames = ["Burger Heroes","Kitchen","Bonsai","Дастархан","Индокитай","X.O","Балкан Гриль","Sherlock Holmes","Speak Easy","Morris Pub","Вкусные истории","Классик","Love&Life","Шок","Бочка"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - Table View data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell
+    }
+    
+
+}
+
